49b1ea7b1b147d74c6ee5f7a787e2c0dd3fa4f58
SAM <8dmasters@gmail.com>
2023-11-05 07:04:49 +0530

    Backend with model deployed
    Sample EEG data are provided and are chosen randomly, due to the unavailibility of EEG Biosensor for model prediction.

--- main.py
+++ main.py
@@ -3,10 +3,41 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 from hashlib import sha256
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
 import flask
 import secrets
 import json
+import random
+import time
+import pickle
+
+
+class eeg_data_collection():
+    def __init__(self, age=10):
+        # self.age = age
+        # eeg_data = eeg.collect().append(self.age)
+
+        self.sample_eeg_data = [
+                [56.0, 43.0, 278.0, 301963.0, 90612.0, 33735.0, 23991.0, 27946.0, 45097.0, 33228.0, 8293.0, 10, 0],
+                [40.0, 35.0, -50.0,	73787.0, 28083.0, 1439.0, 2240.0, 2746.0, 3687.0, 5293.0, 2740.0, 10, 0],
+                [47.0, 57.0, -5.0, 2012240.0, 129350.0, 61236.0, 17084.0, 11488.0, 62462.0, 49960.0, 33932.0, 10, 0]
+            ]
+        
+        self.eeg_data = pd.DataFrame()
+    
+
+    def get_eeg(self):
+        selected = pd.DataFrame(self.sample_eeg_data[random.randint(0, 2)])
+        self.eeg_data = pd.concat([self.eeg_data, selected], ignore_index=True).groupby(level=0).mean()
+    
+
+    def return_eeg_data(self):
+        return self.eeg_data
 
 
 def encode_pass(pwd: str) -> str:
@@ -14,6 +45,16 @@ def encode_pass(pwd: str) -> str:
 
 
 if __name__ == '__main__':
+    start = False
+
+    saved_model = str(Path('__file__').resolve().parent) + r'\Model\Model_Data\Model.h5'
+    saved_stdscaler = str(Path('__file__').resolve().parent) + r'\Model\Model_Data\StandardScaler.pkl'
+
+    model = tf.keras.models.load_model(saved_model)
+
+    with open(saved_stdscaler, 'rb') as f:
+        sc = pickle.load(f)
+
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
@@ -54,6 +95,11 @@ if __name__ == '__main__':
 
     @app.route('/login/', methods=['GET', 'POST'])
     def login():
+        global start
+
+        start = False
+        flask.session['test'] = False
+
         if flask.session['logged'] is True:
             return flask.redirect(flask.url_for('main'))
         
@@ -88,6 +134,10 @@ if __name__ == '__main__':
 
     @app.route('/<name>/')
     def account(name):
+        global start
+
+        flask.session['test'] = False
+        start = False
         if flask.session['logged'] is not False and name == flask.session['name']:
             if Database.query.get(flask.session['id']).ts == json.dumps({}):
                 return flask.redirect(flask.url_for('notification', name=name))
@@ -99,6 +149,7 @@ if __name__ == '__main__':
             }
             
             test_values = list(json.loads(Database.query.get(flask.session['id']).ts).values())
+
             avg = sum(test_values) / len(test_values)
             percent = avg * 10
 
@@ -118,6 +169,10 @@ if __name__ == '__main__':
 
     @app.route('/<name>/notification/', methods=['GET', 'POST'])
     def notification(name):
+        global start
+
+        flask.session['test'] = False
+        start = False
         if flask.session['logged'] is not False and name == flask.session['name']:
             num = len(json.loads(Database.query.get(flask.session['id']).ts)) + 1
 
@@ -131,10 +186,27 @@ if __name__ == '__main__':
 
     @app.route('/<name>/test/', methods=['GET', 'POST'])
     def take_test(name):
+        global start, sc, model, saved_stdscaler
+
         if flask.session['logged'] is not False and name == flask.session['name']:
             if flask.session['test'] is False:
                 return flask.redirect(flask.url_for('main'))
+
+            eeg = eeg_data_collection()
+            start = True
+
+            def eeg_func():
+                global start
+                while start:
+                    eeg.get_eeg()
+                    time.sleep(3)
+
+            socketio.start_background_task(eeg_func)
+
             if flask.request.method == 'POST':
+                flask.session['test'] = False
+                start = False
+
                 corr_ans = ['benny', 'forest', 'picnic', 'sandwich', 'sally']
                 ans = [flask.request.form[f'ans{i}'] for i in range(1, 6)]
 
@@ -142,17 +214,20 @@ if __name__ == '__main__':
 
                 for i, a in enumerate(ans):
                     if corr_ans[i] in a.lower(): scored += 1
-                
-                percent = (scored/5) * 100
+
+                data = eeg.return_eeg_data()
+
+                value = np.squeeze(model.predict([sc.transform(data.to_numpy().T)])) * 30
+                percent = (scored/5) * 70
+
+                percent = round(percent + value, 2)
 
                 data_dict = json.loads(Database.query.get(flask.session['id']).ts)
-                data_dict[f'Test{len(data_dict)}'] = percent/10
+                data_dict[f'Test{len(data_dict)}'] = round(percent/10)
 
                 data_dict = json.dumps(data_dict)
                 Database.query.get(flask.session['id']).ts = data_dict
                 db.session.commit()
-
-                flask.session['test'] = False
 
                 return flask.redirect(flask.url_for('main'))
 
@@ -162,6 +237,11 @@ if __name__ == '__main__':
 
     @app.route('/')
     def main():
+        global start
+
+        flask.session['test'] = False
+        start = False
+
         if flask.session['logged'] is False:
             return flask.redirect(flask.url_for('login'))
         return flask.redirect(flask.url_for('account', name=flask.session['name']))
